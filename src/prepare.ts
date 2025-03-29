948ea8b399bf6f77dacdd0e4b017a23e2e5e7d73
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-03-29 16:19:47 +0900

    feat: output the pull request number (#13)
    * feat: output the pull request number
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/prepare.ts
+++ src/prepare.ts
@@ -2,10 +2,27 @@ import fs from "fs";
 import path from "path";
 import { Buffer } from "buffer";
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { WorkflowRunCompletedEvent } from "@octokit/webhooks-types";
 import * as lib from "./lib";
 
 export const prepare = async (input: lib.Input) => {
   // prepare parse the input data and outputs it as JSON
+  // output pull request number
+  switch (github.context.eventName) {
+    case "workflow_run":
+      const workflowRunEvent = github.context
+        .payload as WorkflowRunCompletedEvent;
+      if (workflowRunEvent.workflow_run.pull_requests.length === 1) {
+        core.setOutput(
+          "pull_request_number",
+          workflowRunEvent.workflow_run.pull_requests[0].number,
+        );
+      }
+    case "workflow_dispatch":
+    case "repository_dispatch":
+    case "label":
+  }
   core.setOutput(
     "ops",
     JSON.stringify(
